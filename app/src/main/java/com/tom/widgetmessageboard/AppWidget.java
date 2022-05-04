0966e7e5ad72b4a99c3aa54408a2637be1dd92f5
Juan <michelle.70623@gmail.com>
2022-05-04 14:36:45 +0800

    init commit

--- app/src/main/java/com/tom/widgetmessageboard/AppWidget.java
+++ app/src/main/java/com/tom/widgetmessageboard/AppWidget.java
@@ -3,48 +3,81 @@ package com.tom.widgetmessageboard;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class AppWidget extends AppWidgetProvider {
-    static String message;
+    // pass image id 陣列
+    static int[] passIds = new int[]{R.drawable.qr_code, R.drawable.quack, R.drawable.main};
+    // pass name 陣列
+    static String[] passName = new String[]{"疫把照 - 疫苗pass", "疫把照 - 檢驗pass", "疫把照 - 康復pass"};
+    // pass 索引，位置（0: vaccine pass, 1: test pass, 2: recovery pass）
+    static int passIndex = 0;
+    private static final String LeftOnClick = "leftOnClickTag";
+    private static final String RightOnClick = "rightOnClickTag";
+    private static final String ImgOnClick = "imgOnClickTag";
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+    protected static PendingIntent getPendingSelfIntent(Context context, String action) {
+        Intent intent = new Intent(context, AppWidget.class);
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
+    }
 
-        CharSequence widgetText = "Message Board";
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
 
-        SharedPreferencesUtils sharedPreferencesUtils = new SharedPreferencesUtils(context);
-        if (sharedPreferencesUtils.getText().equals("")) {
-        } else {
-            message = sharedPreferencesUtils.getText();
-            Log.i("juan", "get sharedPreferences " + message);
+        if (LeftOnClick.equals(intent.getAction())) {
+            if (passIndex > 0) {
+                passIndex--;
+            } else {
+                passIndex = passIds.length - 1;
+            }
+        } else if (RightOnClick.equals(intent.getAction())) {
+            // your onClick action is here
+            if (passIndex < passIds.length - 1) {
+                passIndex++;
+            } else {
+                passIndex = 0;
+            }
         }
-        views.setTextViewText(R.id.tv_messageHistory, message);
-        views.setImageViewResource(R.id.img_enlarge, R.drawable.enlarge);
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        ComponentName thisAppWidget = new ComponentName(context.getPackageName(), AppWidget.class.getName());
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(thisAppWidget);
 
-        Intent intent = new Intent(context, MessageHistoryActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-        views.setOnClickPendingIntent(R.id.img_enlarge, pendingIntent);
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+        onUpdate(context, appWidgetManager, appWidgetIds);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+        // Get all ids
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_widget);
+        ComponentName thisWidget = new ComponentName(context, AppWidget.class);
+        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+
+        Intent intent = new Intent(context, MessageHistoryActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.img, pendingIntent);
+        views.setImageViewResource(R.id.img, passIds[passIndex]);
+        views.setTextViewText(R.id.appwidget_text, passName[passIndex]);
+
+        for (int widgetId : allWidgetIds) {
+            views.setOnClickPendingIntent(R.id.tv_left, getPendingSelfIntent(context, LeftOnClick));
+            views.setOnClickPendingIntent(R.id.tv_right, getPendingSelfIntent(context, RightOnClick));
+
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(widgetId, views);
         }
     }
 
@@ -56,5 +89,13 @@ public class AppWidget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    public void onUpdateWidget(Context context) {
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        ComponentName thisAppWidget = new ComponentName(context.getPackageName(), AppWidget.class.getName());
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(thisAppWidget);
+        passIndex = 0;
+        onUpdate(context, appWidgetManager, appWidgetIds);
     }
 }
--- app/src/main/java/com/tom/widgetmessageboard/MainActivity.java
+++ app/src/main/java/com/tom/widgetmessageboard/MainActivity.java
@@ -2,18 +2,31 @@ package com.tom.widgetmessageboard;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText text;
-    private Button enter;
-    private SharedPreferencesUtils sharedPreferencesUtils;
+    private ImageView img;
+    private TextView tv;
+    // 手勢偵測器
+    private GestureDetector detector;
+    // pass image id 陣列
+    private int[] passIds;
+    // pass name 陣列
+    private String[] passName;
+    // pass 索引，位置（0: vaccine pass, 1: test pass, 2: recovery pass）
+    private int passIndex;
+    private Button btnUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,26 +34,74 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initialize();
-        enter.setOnClickListener(new View.OnClickListener() {
+
+        passIds = new int[]{R.drawable.qr_code, R.drawable.quack, R.drawable.main};
+        passName = new String[]{"疫把照 - 疫苗pass", "疫把照 - 檢驗pass", "疫把照 - 康復pass"};
+        detector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent motionEvent) {
+                return false;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent motionEvent) {
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent motionEvent) {
+                return false;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                return false;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent motionEvent) {
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                // 手勢往左滑動
+                if (e2.getX() < e1.getX() - 40) {
+                    Log.i("juan", "左移");
+                    if (passIndex < passIds.length - 1) {
+                        passIndex++;
+                    } else {
+                        passIndex = 0;
+                    }
+                }
+                // 手勢往右滑動
+                if (e2.getX() > e1.getX() + 40) {
+                    Log.i("juan", "右移");
+                    if (passIndex > 0) {
+                        passIndex--;
+                    } else {
+                        passIndex = passIds.length - 1;
+                    }
+                }
+                img.setImageResource(passIds[passIndex]);
+                tv.setText(passName[passIndex]);
+                return false;
+            }
+        });
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (text.getText().equals("") || text.getText() == null) {
-                    Toast.makeText(MainActivity.this, "Please enter some words!", Toast.LENGTH_SHORT).show();
-                } else {
-                    String message = text.getText().toString();
-                    sharedPreferencesUtils.setMessageHistory(message);
-                    sharedPreferencesUtils.setText(message);
-//                    Intent intent = new Intent(MainActivity.this, AppWidget.class);
-//                    intent.putExtra("message", message);
-//                    sendBroadcast(intent);
-                }
+                new AppWidget().onUpdateWidget(MainActivity.this);
             }
         });
     }
 
+    public boolean onTouchEvent(MotionEvent event) {
+        return detector.onTouchEvent(event);
+    }
+
     public void initialize() {
-        text = findViewById(R.id.et_text);
-        enter = findViewById(R.id.btn_enter);
-        sharedPreferencesUtils = new SharedPreferencesUtils(this);
+        img = findViewById(R.id.img);
+        tv = findViewById(R.id.tv);
+        btnUpdate = findViewById(R.id.btn_update);
     }
 }
